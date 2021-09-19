972bd176fe654541a808e57b309cd224db96c84c
Aliecco <79269942+Aliecco@users.noreply.github.com>
2021-09-20 00:14:01 +0300

    Update arab.java

--- JavaMentor/src/testpack/arab.java
+++ JavaMentor/src/testpack/arab.java
@@ -1,4 +1,22 @@
 package testpack;
 
 public class arab {
+  public static int Arab(int a, int b, char c){   //тут просто калькулятор
+        int res=0;
+        switch (c){
+            case '-':
+                res=a-b;
+                break;
+            case '+':
+                res=a+b;
+                break;
+            case '*':
+                res=a*b;
+                break;
+            case '/':
+                res=a/b;
+                break;
+        }
+        return res;
+    }
 }
