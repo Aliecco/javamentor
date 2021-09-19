5ad2597b4e30d4d8969d9f16caa79d455a064bdb
Aliecco <79269942+Aliecco@users.noreply.github.com>
2021-09-20 00:13:20 +0300

    Update Main.java

--- JavaMentor/src/testpack/Main.java
+++ JavaMentor/src/testpack/Main.java
@@ -5,48 +5,69 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         Scanner scn = new Scanner(System.in);
-        String exp= new String(scn.nextLine());
+        String exp= scn.nextLine();
         System.out.println(exp);
         char [] charsArr = exp.toCharArray();
-        char a,b, ch=' ';
+        char ch=' ';
         boolean math=false;
-        int len = 1;
-        for(int i=0;i<exp.length();i++){
-            if(charsArr[i]=='-'){
-                ch =charsArr[i];
-                math=true;
-                len++;
+        int a=0, b=0, len = 1;
+        for(int i=0;i<exp.length();i++){    //Нахожу мат оператор
+            switch (charsArr[i]){
+                case '-':
+                case '+':
+                case '*':
+                case '/':
+                    ch =charsArr[i];
+                    math=true;              //проверка есть ли мат оператор
+                    len++;                  //кол-мат операторов, должно быть два операнда и один оператор
+                    break;
             }
-            else if(charsArr[i]=='+'){
-                ch =charsArr[i];
-                math=true;
-                len++;
-            }
-            else if(charsArr[i]=='*'){
-                ch =charsArr[i];
-                math=true;
-                len++;
-            }
-            else if(charsArr[i]=='/'){
-                ch =charsArr[i];
-                math=true;
-                len++;
-            }
-
-
         }
+
         String asd = String.valueOf(ch);
-        String[] nums = new String[len];
-        if(math&&len==2) {
-            nums = exp.split(asd);
-            System.out.println(nums[0]);
-            System.out.println(nums[1]);
+        String[] numbs = new String[len];
+        if(math&&len==2) {                  //разделяю на 2 строки
+            switch (asd){
+                case "-":
+                case "/":
+                    numbs = exp.split(asd);
+                    break;
+                case "*":
+                    numbs = exp.split("\\*");
+                    break;
+                case "+":
+                    numbs = exp.split("\\+");
+            }
+            System.out.println(numbs[0]);
+            System.out.println(numbs[1]);
         }
         else {
             System.out.println("Не математическая функция");
+        }
+        int z=0;
+        try {
+            a = Integer.parseInt(numbs[0]);     //определяю арабские или римские цифры
+            z++;
+        }
+        catch (NumberFormatException ex){
+        }
+        try {
+            b = Integer.parseInt(numbs[1]);
+            z++;
+        }
+        catch (NumberFormatException ex){
+        }
+        switch (z){
+            case 2:
+                System.out.println(arab.Arab(a,b,ch));
+                break;
+            case 0:
+                System.out.println(rome.Rome(numbs[0],numbs[1],ch));
+                break;
+            default:
+                System.out.println("Ошибка");
         }
 
     }
